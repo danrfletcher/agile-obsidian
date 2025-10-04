087543a2beb810fda711125cbb499b96ed569cb1
danrfletcher <danrfletcher@outlook.com>
2025-10-04 18:58:32 +0300

    chore: update platform barrel to export repo

--- src/platform/obsidian/index.ts
+++ src/platform/obsidian/index.ts
@@ -2,3 +2,4 @@ export { createObsidianAppAdapter } from "./app/app-adapter";
 export { getCursorContext } from "./editor/obsidian-editor-context";
 export * from "./fs/fs-utils";
 export * from "./editor/editor-context-utils";
+export { createPathFileRepository } from "./fs/file-repository";
